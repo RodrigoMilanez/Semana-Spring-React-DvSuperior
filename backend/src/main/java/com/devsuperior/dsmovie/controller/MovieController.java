06454944be0eeb67b387013061fa50db8eeb6cd4
Rodrigo Milanez <rodrigo.ohland@hotmail.com>
2022-03-09 15:08:08 -0300

    Controller e service

---
+++ backend/src/main/java/com/devsuperior/dsmovie/controller/MovieController.java
@@ -0,0 +1,24 @@
+package com.devsuperior.dsmovie.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.devsuperior.dsmovie.dto.MovieDTO;
+import com.devsuperior.dsmovie.service.MovieService;
+
+@RestController
+@RequestMapping(value = "/movies")
+public class MovieController {
+
+	@Autowired
+	private MovieService movieService;
+	
+	@GetMapping
+	public Page<MovieDTO> findAll(Pageable pageable){
+		return movieService.findListAll(pageable);
+	}
+}
