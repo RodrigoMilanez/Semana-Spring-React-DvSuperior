5dcba9332e19f00052f1e0b3ac10bccbe97db7f3
Rodrigo Milanez <rodrigo.ohland@hotmail.com>
2022-03-11 15:47:14 -0300

    requisição exibida na interface

--- frontend/src/pages/listing/index.tsx
+++ frontend/src/pages/listing/index.tsx
@@ -1,12 +1,11 @@
+import axios from 'axios';
+import MovieCard from 'components/MovieCard';
 import Pagination from 'components/Pagination';
 import { useEffect, useState } from 'react';
-import MovieCard from 'components/MovieCard';
-import axios from 'axios';
+import { MoviePage } from 'Types/movie';
 import { BASE_URL } from 'utils/requests';
-import { MoviePage } from 'types/movie';
-
-
 import './styles.css';
+
 
 
 
@@ -14,12 +13,41 @@ function Listing() {
 
     const [pageNumber, setPageNumber] = useState(0);
 
+    const [page, setPage] = useState<MoviePage>(
+        {
+            content: [],
+            last: true,
+            totalPages: 0,
+            totalElements: 0,
+            size: 12,
+            number: 0,
+            first: true,
+            numberOfElements: 0,
+            empty: true
+        }
+    );
+
     useEffect(() => {
-        axios.get(`${BASE_URL}/movies?size=12&page=0`).then(response => {
-            const data= response.data as MoviePage;
-            setPageNumber(data.number);
+        axios.get(`${BASE_URL}/movies?size=12&page=${pageNumber}`).then(response => {
+            const data = response.data as MoviePage;
+            setPage(data);
         })
-    }, [])
+    }, [pageNumber])
+
+    axios.get(`${BASE_URL}/movies?size=12&page=0`).then(response => {
+        const data = response.data as MoviePage;
+        setPageNumber(data.number);
+    })
+
+
+
+    const movie = {
+        id: 1,
+        image: "https://www.themoviedb.org/t/p/w533_and_h300_bestv2/jBJWaqoSCiARWtfV0GlqHrcdidd.jpg",
+        title: "The Witcher",
+        count: 2,
+        score: 4.5
+    };
 
     return (
 
@@ -27,27 +55,13 @@ function Listing() {
             <Pagination />
             <div className="container">
                 <div className="row">
-                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-                        <MovieCard />
-                    </div>
-                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-                        <MovieCard />
-                    </div>
-                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-                        <MovieCard />
-                    </div>
-                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-                        <MovieCard />
-                    </div>
-                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-                        <MovieCard />
-                    </div>
-                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-                        <MovieCard />
-                    </div>
-                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-                        <MovieCard />
-                    </div>
+                    {page.content.map(movie => 
+                        <div key={movie.id} className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                            <MovieCard movie={movie} />
+                        </div>
+                    )}
+
+
 
                 </div>
             </div>
